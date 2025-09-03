472faaf59cdb81e9c118baba671f99429d03b09a
Radian-Motorsport <jdotsowerby@gmail.com>
2025-09-03 16:34:21 +0100

    Delete renderer.js

--- renderer.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getStatus: () => ipcRenderer.invoke('get-status'),
-  minimizeWindow: () => ipcRenderer.send('minimize-window')
-});
-
